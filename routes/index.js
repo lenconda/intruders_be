820c179c92280676623d4f54a864ad6f1d9b9968
lenconda <1053464288@qq.com>
2018-08-12 22:09:39 +0800

    Remove koa2 and migrate to express

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const Router = require('koa-router')
-const router = new Router()
-
-router.get('/', async (ctx, next) => {
-  ctx.body = 'Welcome to Koa2 Lenconda Template!'
-})
-
-module.exports = router
