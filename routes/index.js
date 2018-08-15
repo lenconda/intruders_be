edf0991418d1c130f6b7c4c20e1d53ef200321b4
lenconda <1053464288@qq.com>
2018-08-15 23:53:01 +0800

    Added views

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express')
 var router = express.Router()
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.send({ status: 1, message: '请求成功' })
+router.get('/', function(req, res, next) {
+  res.send({ status: 1, message: 'OK' })
 })
 
 module.exports = router
