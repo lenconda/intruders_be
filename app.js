dd110430cee1b275d95a6dd2402957672306c4a1
lenconda <1053464288@qq.com>
2018-08-17 22:30:47 +0800

    🎨fix(views): Remove view templates

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var createError = require('http-errors')
+// var createError = require('http-errors')
 var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
@@ -25,9 +25,9 @@ app.use('/search', searchRouter)
 app.use('/hotspot', hotspotRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404))
-})
+// app.use(function(req, res, next) {
+//   next(createError(404))
+// })
 
 // error handler
 app.use(function(err, req, res, next) {
