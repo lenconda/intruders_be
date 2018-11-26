d4caef84cfc5e3a0ce81171a6f41e5ab863444de
lenconda <1053464288@qq.com>
2018-11-26 17:27:31 +0800

    fix(return)

--- app/controllers/index.ts
+++ app/controllers/index.ts
@@ -2,11 +2,10 @@ import { JsonController, Get } from 'routing-controllers'
 
 @JsonController('/')
 export default class IndexController {
+
   @Get('')
   async hello() {
-    return {
-      status: 1,
-      message: 'OK'
-    }
+    return {}
   }
+
 }
